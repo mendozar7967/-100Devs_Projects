e25986e7531ab7453fe7cbc7881eefc63a2cbe08
mendozar7967 <mendozar7967@gmail.com>
2022-05-23 23:15:53 -0500

    Added Google maps integration and updated reviews

--- Mexican Restaurant/main.js
+++ Mexican Restaurant/main.js
@@ -11,3 +11,22 @@ function dropDown(){
     menuFind.classList.toggle('hidden')
     menuOrder.classList.toggle('hidden')
 }
+
+// Initialize and add the map
+function initMap() {
+    // The location of El Pueblo Mexican Grill
+    const elPuebloMexicanGrill = { lat: 41.51232367854285, lng: -90.4171953107478 }; //41.51232367854285, -90.4171953107478
+    // The map, centered at El Pueblo Mexican Grill
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 17,
+      center: elPuebloMexicanGrill,
+    });
+    // The marker, positioned at El Pueblo Mexican Grill
+    const marker = new google.maps.Marker({
+      position: elPuebloMexicanGrill,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
+  
